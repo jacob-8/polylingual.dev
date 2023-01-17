f5d034ffa1beb96211023a9a9f1ce13c20d66389
jacob-8 <jwrunner7@gmail.com>
2023-01-17 08:24:19 +0800

    Add Filetree

---
+++ src/lib/filetree/File.svelte
@@ -0,0 +1,72 @@
+<script>
+	import * as context from './context.js';
+	import Item from './Item.svelte';
+
+	/** @type {import('$lib/types').FileStub} */
+	export let file;
+
+	const { selected, endstate, select, rename, remove, readonly } = context.get();
+
+	/** @type {'idle' | 'renaming'} */
+	let state = 'idle';
+
+	$: can_remove = !$readonly && !$endstate[file.name];
+
+	/** @type {import('./ContextMenu.svelte').MenuItems} */
+	$: actions = can_remove
+		? [
+				{
+					icon: 'rename',
+					label: 'Rename',
+					fn: () => {
+						state = 'renaming';
+					}
+				},
+				{
+					icon: 'delete',
+					label: 'Delete',
+					fn: () => {
+						remove(file);
+					}
+				}
+		  ]
+		: [];
+</script>
+
+<div class="row" class:selected={file === $selected}>
+	<Item
+		can_rename={can_remove}
+		renaming={state === 'renaming'}
+		basename={file.basename}
+		{actions}
+		on:click={() => select(file)}
+		on:edit={() => {
+			state = 'renaming';
+		}}
+		on:rename={(e) => {
+			rename(file, e.detail.basename);
+		}}
+		on:cancel={() => {
+			state = 'idle';
+		}}
+	/>
+</div>
+
+<style>
+	.selected {
+		color: var(--prime) !important;
+	}
+
+	.selected::after {
+		content: '';
+		position: absolute;
+		width: 1rem;
+		height: 1rem;
+		top: 0.1rem;
+		right: calc(-2.6rem - 2px);
+		background-color: var(--sk-back-1);
+		border: 1px solid var(--sk-back-4);
+		transform: translate(0, 0.2rem) rotate(45deg);
+		z-index: 2;
+	}
+</style>
--- src/routes/tutorial/[slug]/+page.svelte
+++ src/routes/tutorial/[slug]/+page.svelte
@@ -5,11 +5,54 @@
   import Sidebar from './Sidebar.svelte';
   import Stackblitz from './Stackblitz.svelte';
   import MonacoEditor from '$lib/monaco/MonacoEditor.svelte';
-  import type { FileStub } from '$lib/types';
+  import type { Adapter, EditingConstraints, Exercise, FileStub, Scope, Stub } from '$lib/types';
   import type { PageData } from './$types';
+  import Filetree from '$lib/filetree/Filetree.svelte';
   export let data: PageData;
 
+  let path = data.exercise.path;
+  const files = writable<Stub[]>([]);
+  const endstate = writable<Record<string, Stub>>({});
   const selected = writable<FileStub | null>(null);
+  const scope = writable<Scope>({ depth: 0, name: '', prefix: '' });
+  const editing_constraints = writable<EditingConstraints>({ create: [], remove: [] });
+
+  let adapter: Adapter | undefined;
+
+  $: new_exercise(data.exercise);
+  function new_exercise(exercise: Exercise) {
+    $files = Object.values(exercise.a);
+    $scope = exercise.scope;
+    path = exercise.path;
+    selected.set($files.find((stub) => stub.name === exercise.focus) as FileStub);
+    if (exercise.meta.editing_constraints) $editing_constraints = exercise.meta.editing_constraints;
+    $endstate = { ...exercise.a };
+
+    for (const stub of Object.values(exercise.b)) {
+      if (!$endstate[stub.name] && !$editing_constraints.create.includes(stub.name)) {
+        $editing_constraints.create.push(stub.name);
+      }
+      $endstate[stub.name] = exercise.b[stub.name];
+    }
+  }
+
+  // Loads the adapter initially or resets it. This method can throw.
+  async function reset_adapter(stubs: Stub[]) {
+    let reload_iframe = true;
+    if (adapter) {
+      reload_iframe = await adapter.reset(stubs);
+    } else {
+      // @ts-ignore
+      adapter = {};
+      set_iframe_src(adapter?.base + path);
+    }
+    if (reload_iframe) {
+      set_iframe_src(adapter?.base + path);
+    }
+    return adapter;
+  }
+
+  function set_iframe_src(url: string) {}
 </script>
 
 <SplitPane pos={33} min={0}>
@@ -19,9 +62,7 @@
       exercise={data.exercise}
       on:select={(e) => {
         const file = data.exercise.a[e.detail.file];
-        if (file.type === 'file') {
-          selected.set(file);
-        }
+        if (file.type === 'file') selected.set(file);
       }}
     />
   </section>
@@ -29,7 +70,18 @@
     <SplitPane pos={50} type="vertical">
       <section class="h-full" slot="a">
         <SplitPane pos={27}>
-          <section class="bg-blue/25 h-full" slot="a">File tree</section>
+          <section class="h-full" slot="a">
+            <Filetree
+              {scope}
+              {endstate}
+              {files}
+              constraints={editing_constraints}
+              {selected}
+              on:change={() => {
+                reset_adapter($files);
+              }}
+            />
+          </section>
           <section class="bg-green/25 h-full" slot="b">
             <MonacoEditor stubs={[]} />
           </section>
