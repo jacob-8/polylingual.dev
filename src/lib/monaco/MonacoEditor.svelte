c8732266537ed541121c5752210959b4dc628710
jacob-8 <jwrunner7@gmail.com>
2023-01-18 20:19:53 +0800

    simplify monaco and add vs-dark-plus (but it needs textmate first)

--- src/lib/monaco/MonacoEditor.svelte
+++ src/lib/monaco/MonacoEditor.svelte
@@ -2,12 +2,13 @@
   import { dev } from '$app/environment';
   import type { FileStub, Stub } from '$lib/types';
   import { createEventDispatcher, onMount } from 'svelte';
-  import { svelteLight, svelteDark } from './monaco-themes.js';
+  import { vs_dark_plus } from './monaco-themes.js';
 
   const mapOfExtensionToLanguage: Record<string, string> = {
     js: 'javascript',
     ts: 'typescript',
-    svelte: 'html', // TODO
+    svelte: 'html',
+    md: 'markdown', // TODO
   };
 
   const mapOfURLtoModel = new Map<string, import('monaco-editor').editor.ITextModel>();
@@ -20,17 +21,6 @@
   let container: HTMLDivElement;
   let instance: ReturnType<typeof init> | undefined;
 
-  let width = 0;
-  let height = 0;
-
-  /**
-   * The iframe sometimes takes focus control in ways we can't prevent
-   * while the editor is focussed. Refocus the editor in these cases.
-   * This boolean tracks whether or not the editor should be refocused.
-   */
-  let preserve_focus = true;
-  let remove_focus_timeout: any;
-
   onMount(() => {
     let destroyed = false;
 
@@ -40,42 +30,31 @@
       return;
     }
 
-    let dark_mode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-
     import('./load-monaco.js').then(({ monaco }) => {
       if (destroyed) return;
-      instance = init(monaco, dark_mode);
+      instance = init(monaco);
     });
-
-    const on_mode_change = (event: MediaQueryListEvent) => {
-      const dark = event.matches;
-      if (dark !== dark_mode) {
-        dark_mode = dark;
-        instance?.set_theme(dark ? 'svelte-dark' : 'svelte');
-      }
-    };
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', on_mode_change);
 
     return () => {
       destroyed = true;
-      window
-        .matchMedia('(prefers-color-scheme: dark)')
-        .removeEventListener('change', on_mode_change);
       if (instance) {
-        instance.update_files([]); // removes all files
+        const VALUE_TO_REMOVE_ALL_FILES: [] = [];
+        instance.update_files(VALUE_TO_REMOVE_ALL_FILES);
         instance.editor.dispose();
       }
     };
   });
 
-  function init(monaco: typeof import('monaco-editor'), dark_mode: boolean) {
-    monaco.editor.defineTheme('svelte', svelteLight);
-    monaco.editor.defineTheme('svelte-dark', svelteDark);
-    monaco.editor.setTheme(dark_mode ? 'svelte-dark' : 'svelte');
+  function init(monaco: typeof import('monaco-editor')) {
+    monaco.editor.defineTheme('vs-dark-plus', vs_dark_plus);
+    monaco.editor.setTheme('vs-dark-plus');
+    // if adding textmate grammar using https://github.com/zikaari/monaco-editor-textmate https://github.com/ChristopherHButler/vscode-themes-in-monaco/blob/d2ad1847c765bf03ad22853132e8c96c6758c680/src/components/CodeEditor.js#L170 - then the vs-dark-plus theme will have additional effect
 
     const editor = monaco.editor.create(container, {
+      automaticLayout: true,
+      lineNumbersMinChars: 3,
       fontFamily: 'Roboto Mono',
-      fontSize: 13,
+      fontSize: 15,
       padding: {
         top: 16,
         bottom: 16,
@@ -170,52 +149,16 @@
     const model = selected && mapOfURLtoModel.get(selected.name);
     instance.editor.setModel(model ?? null);
   }
-
-  $: if (instance && (width || height)) instance.editor.layout();
 </script>
 
-<svelte:window
-  on:message={(e) => {
-    if (preserve_focus && e.data.type === 'focus_on_editor') {
-      instance?.editor.focus();
-    }
-  }}
-/>
-
-<div class="w-full h-full" bind:clientWidth={width} bind:clientHeight={height}>
-  <div
-    class="w-full h-full"
-    bind:this={container}
-    on:focusin={() => {
-      clearTimeout(remove_focus_timeout);
-      preserve_focus = true;
-    }}
-    on:focusout={() => {
-      // Heuristic: user did refocus themmselves if focus_on_editor
-      // doesn't happen in the next few miliseconds. Needed
-      // because else navigations inside the iframe refocus the editor.
-      remove_focus_timeout = setTimeout(() => {
-        preserve_focus = false;
-      }, 500);
-    }}
-  />
-</div>
+<div class="w-full h-full" bind:this={container} />
 
 <style>
-  /* TODO figure out how to make the indent guides
-	   play nicely with fonts other than Menlo */
-  div :global(.core-guide-indent.vertical),
-  div :global(.core-guide-indent-active.vertical) {
+  div :global(.core-guide-indent.vertical) {
     display: none;
   }
 
-  /* TODO figure out how to remove the weird 1px line
-	   in the scroll gutter */
   div :global(.decorationsOverviewRuler) {
     display: none !important;
-  }
-
-  div :global(.monaco-editor .view-overlays .current-line) {
-    border: none;
   }
 </style>
