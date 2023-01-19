6c3b5263f95daedc9badd6378e888d5909684bb5
jacob-8 <jwrunner7@gmail.com>
2023-01-19 10:55:00 +0800

    refactor out Content into own component

---
+++ src/routes/tutorial/[slug]/Content.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import type { Exercise, FileStub } from '$lib/types';
+  import { createEventDispatcher } from 'svelte';
+  
+  export let exercise: Exercise;
+  const dispatch = createEventDispatcher<{ selected: { file: FileStub } }>();
+
+  function encourage_not_to_copy(e: ClipboardEvent) {
+    const namespace = 'learn.polylingual.dev';
+    const copy_enabled = `${namespace}:copy_enabled`;
+
+    if (sessionStorage[copy_enabled]) return;
+
+    let node = e.target as HTMLElement;
+
+    while (node && node !== e.currentTarget) {
+      if (node.nodeName === 'PRE') {
+        const enable_copy = confirm(
+          'Copy and paste is disabled We recommend typing the code into the editor to complete the exercise, as this results in better learning. Do you want to enable copy-and-paste for the duration of this session?'
+        );
+        if (enable_copy) sessionStorage[copy_enabled] = enable_copy;
+
+        e.preventDefault();
+        return;
+      }
+
+      node = node.parentNode as HTMLElement;
+    }
+  }
+
+  function respond_to_file_name_clicked(e: MouseEvent) {
+    const node = e.target as HTMLElement;
+
+    if (node.nodeName === 'CODE') {
+      const { file: filename } = node.dataset;
+      if (filename) {
+        const file = exercise.a[filename];
+        if (file.type === 'file') dispatch('selected', { file });
+      }
+    }
+  }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div on:click={respond_to_file_name_clicked} on:copy={encourage_not_to_copy}>
+  {@html exercise.html}
+</div>
+
+<style>
+  :global(pre) {
+    overflow-x: auto;
+  }
+  /* .text :global(pre) :global(.highlight) {
+    --color: rgba(220, 220, 0, 0.2);
+    background: var(--color);
+    outline: 2px solid var(--color);
+    border-radius: 2px;
+  }
+
+  .text :global(pre) :global(.highlight.add) {
+    --color: rgba(0, 255, 0, 0.1);
+  }
+
+  .text :global(pre) :global(.highlight.remove) {
+    --color: rgba(255, 0, 0, 0.1);
+  } */
+</style>
--- src/routes/tutorial/[slug]/Sidebar.svelte
+++ src/routes/tutorial/[slug]/Sidebar.svelte
@@ -1,64 +1,24 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
   import { afterNavigate } from '$app/navigation';
   import Menu from './Menu.svelte';
-  import type { Exercise, FileStub, PartStub } from '$lib/types';
+  import Content from './Content.svelte';
+  import type { Exercise, PartStub } from '$lib/types';
 
   export let tree: PartStub[];
   export let exercise: Exercise;
-
-  const dispatch = createEventDispatcher<{ selected: { file: FileStub } }>();
-
-  const namespace = 'learn.polylingual.dev';
-  const copy_enabled = `${namespace}:copy_enabled`;
 
   let sidebar: HTMLElement;
 
   afterNavigate(async () => {
     sidebar.scrollTop = 0;
   });
-
-  function encourage_not_to_copy(e: ClipboardEvent) {
-    if (sessionStorage[copy_enabled]) return;
-
-    let node = e.target as HTMLElement;
-
-    while (node && node !== e.currentTarget) {
-      if (node.nodeName === 'PRE') {
-        const enable_copy = confirm(
-          'Copy and paste is disabled We recommend typing the code into the editor to complete the exercise, as this results in better learning. Do you want to enable copy-and-paste for the duration of this session?'
-        );
-        if (enable_copy) sessionStorage[copy_enabled] = enable_copy;
-
-        e.preventDefault();
-        return;
-      }
-
-      node = node.parentNode as HTMLElement;
-    }
-  }
-
-  function respond_to_file_name_clicked(e: MouseEvent) {
-    const node = e.target as HTMLElement;
-
-    if (node.nodeName === 'CODE') {
-      const { file: filename } = node.dataset;
-      if (filename) {
-        const file = exercise.a[filename];
-        if (file.type === 'file') dispatch('selected', { file });
-      }
-    }
-  }
 </script>
 
 <Menu {tree} current={exercise} />
 
 <div class="overflow-y-auto grow flex flex-col">
-  <main bind:this={sidebar} class="p-3" on:copy={encourage_not_to_copy}>
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div on:click={respond_to_file_name_clicked}>
-      {@html exercise.html}
-    </div>
+  <main bind:this={sidebar} class="p-3">
+    <Content {exercise} on:selected />
 
     {#if exercise.next}
       <div class="my-4">
@@ -82,23 +42,3 @@
     </a>
   </footer>
 </div>
-
-<style>
-  :global(pre) {
-    overflow-x: auto;
-  }
-  /* .text :global(pre) :global(.highlight) {
-    --color: rgba(220, 220, 0, 0.2);
-    background: var(--color);
-    outline: 2px solid var(--color);
-    border-radius: 2px;
-  }
-
-  .text :global(pre) :global(.highlight.add) {
-    --color: rgba(0, 255, 0, 0.1);
-  }
-
-  .text :global(pre) :global(.highlight.remove) {
-    --color: rgba(255, 0, 0, 0.1);
-  } */
-</style>
