f7d06a6490bdc5e74afc017db23fbac7d6d7d282
jacob-8 <jwrunner7@gmail.com>
2023-03-06 07:35:50 +0800

    add-prepare-docs-for-search

---
+++ packages/prepare-docs-for-search/process/flatten_sections.ts
@@ -0,0 +1,85 @@
+import { Section } from "../types";
+
+export function flatten_sections(sections: Section[], parent_titles: string[] = []): Section[] {
+  return sections.reduce((acc, section) => {
+    const { title, children, ...rest } = section;
+    const section_titles = title ? [...parent_titles, title] : parent_titles;
+    if (rest.content) acc.push({ ...rest, title, combined_title: section_titles.join(' > ') });
+    if (children) {
+      acc.push(...flatten_sections(children, section_titles));
+    }
+    return acc;
+  }, [] as Section[]);
+};
+
+if (import.meta.vitest) {
+  describe('flatten_sections', () => {
+    test('returns an array with sections flattened when given an array with sections with children and concats titles', () => {
+      const sections: Section[] = [
+        {
+          title: 'Section 1',
+          content: 'Section 1 content',
+          children: [
+            {
+              title: 'Section 1.1',
+              content: 'Section 1.1 content',
+            },
+            {
+              title: 'Section 1.2',
+              content: 'Section 1.2 content',
+              children: [
+                {
+                  title: 'Section 1.2.1',
+                  content: 'Section 1.2.1 content',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          title: 'Section 2',
+          content: 'Section 2 content',
+        },
+      ];
+      const result = flatten_sections(sections);
+      expect(result).toEqual([
+        {
+          title: 'Section 1',
+          combined_title: 'Section 1',
+          content: 'Section 1 content',
+        },
+        {
+          title: 'Section 1.1',
+          combined_title: 'Section 1 > Section 1.1',
+          content: 'Section 1.1 content',
+        },
+        {
+          title: 'Section 1.2',
+          combined_title: 'Section 1 > Section 1.2',
+          content: 'Section 1.2 content',
+        },
+        {
+          title: 'Section 1.2.1',
+          combined_title: 'Section 1 > Section 1.2 > Section 1.2.1',
+          content: 'Section 1.2.1 content',
+        },
+        {
+          title: 'Section 2',
+          combined_title: 'Section 2',
+          content: 'Section 2 content',
+        },
+      ]);
+    });
+
+    test('returns an empty array when given an empty array', () => {
+      const sections: Section[] = [];
+      const result = flatten_sections(sections);
+      expect(result).toEqual([]);
+    });
+
+    test('skips section if it has no content', () => {
+      const sections: Section[] = [{ title: 'No Content', content: '', },];
+      expect(flatten_sections(sections)).toEqual([]);
+    });
+  });
+}
